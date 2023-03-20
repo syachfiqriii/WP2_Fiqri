ce78db95e7de76a1ac5a6850695e9758bf6f63fb
syachfiqriii <syachfiqri@gmail.com>
2023-03-20 09:55:29 +0700

    first commit

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan<h1>";
+        echo "Nama Saya Muhammad Syah Fiqri
+            Saya tinggal di daerah ciputat
+            olah raga yang saya sukai adalah
+            Futsal";
+    }
+}
